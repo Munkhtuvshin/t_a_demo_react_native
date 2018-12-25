dc9059fc2cca4c2956c48b313bcd2d73373ae2b7
PC <monhood34@gmail.com>
2018-12-25 16:23:24 +0800

    qrcode scanner add

--- src/modules/movie/component/MovieDetail.js
+++ src/modules/movie/component/MovieDetail.js
@@ -1,12 +1,13 @@
 'use strict';
 
 import React, { Component } from 'react';
-
+import QRCodeScanner from 'react-native-qrcode-scanner';
 import {
   StyleSheet,
   View,
   Text,
   TouchableOpacity,
+  Linking
 } from 'react-native';
 
 class MovieDetail extends Component {
@@ -17,6 +18,16 @@ class MovieDetail extends Component {
   onBack = () => {
     this.props.navigation.goBack()
   }
+  onSuccess(e) {
+    alert(e.data.toString())
+    // this.scanner.reactivate() //dahin scan
+    // this.scanner.reactivate()
+    // Linking
+    //   .openURL(e.data)
+    //   .catch(err => console.error('An error occured', err));
+  }
+
+  // _reactivate = throttle(() => { if (this.scanner) this.scanner.reactivate(); }, 2000, {trailing: true, leading: false});
 
   // constructor(props) {
   //   // this.props.getMovieDetail(movie)
@@ -40,6 +51,20 @@ class MovieDetail extends Component {
         
         <View style={styles.bodyContainer}>
           <Text style={styles.description}>{'Ene kinonii id : '+this.props.movie.data.id}</Text>
+          <QRCodeScanner
+          ref={(node) => { this.scanner = node }}
+          onRead={this.onSuccess.bind(this)}
+          topContent={
+            <Text style={styles.centerText}>
+              Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+            </Text>
+          }
+          bottomContent={
+            <TouchableOpacity style={styles.buttonTouchable}>
+              <Text style={styles.buttonText}>OK. Got it!</Text>
+            </TouchableOpacity>
+          }
+        />
         </View>        
         
       </View>
@@ -78,7 +103,24 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 17,
     fontWeight: 'bold',
-  }
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
 });
 
 
